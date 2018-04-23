848eaf8bd4de4438d90bc45547bebcb6430c7525
mau5atron <gabrielbetancourt96@outlook.com>
2018-04-23 14:54:26 -0700

    maps in maps

---
+++ maps_in_maps.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+	// We can store multiple items in a map
+
+	superhero := map[string]map[string]string{
+		"Superman":map[string]string{
+			"realname":"Clark Kent",
+			"city":"Metropolis",
+		},
+		"Batman": map[string]string{
+			"realname":"Bruce Wayne",
+			"city":"Gotham City",
+		},
+	}
+
+	// We can output data where the key matches Superman
+
+	if temp, hero := superhero["Superman"]; hero {
+		fmt.Println(temp["realname"], temp["city"])
+	}
+}
