2a91fa2d181b2abd8ed0b4c69ee61738b263d33f
mau5atron <gabrielbetancourt96@outlook.com>
2018-04-23 06:45:42 -0700

    Inital commit

---
+++ intro_datatypes_arthmetic.go
@@ -0,0 +1,27 @@
+// Go programs start with a package declaration to make the code resuable
+
+package main 
+
+
+// Import allows the use of other packages 
+// The format package provides formatting for input and output
+import "fmt"
+
+// A comment
+/*
+	Multiline comment
+*/ 
+
+// Functions start with func and surround the code with {}
+// main is the function that is executed when you execute your program
+
+func main(){
+
+	// Println is a function in the fmt package that outputs a string, 
+	// which is surrounded by double quotes and a newline to the screen
+
+
+
+
+
+}
